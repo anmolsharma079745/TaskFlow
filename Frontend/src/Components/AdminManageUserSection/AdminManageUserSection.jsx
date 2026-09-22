875bf734fb0553f9f1e73e2134856d259d54f031
Anmol2005 <asharma91975@gmail.com>
2026-09-22 10:46:02 +0530

    Fix AdminManageSection folder casing

--- Frontend/src/Components/AdminManageUserSection/AdminManageUserSection.jsx
+++ Frontend/src/Components/AdminManageUserSection/AdminManageUserSection.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import "./AdminMAnageUserSection.css";
+import "./AdminManageUserSection.css";
 
 const AdminUsers = () => {
 
